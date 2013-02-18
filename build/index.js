064155f7d79c49257a1690d58f93414affd10234
Bjorn Stromberg <bjornstar@gmail.com>
2013-02-18 15:13:04 +0900

    create public directory on build.

--- build/index.js
+++ build/index.js
@@ -1,12 +1,14 @@
 var Builder = require('component-builder');
 var fs      = require('fs');
-var write = fs.writeFileSync;
+var write   = fs.writeFileSync;
+var mkdir   = fs.mkdirSync;
 
 module.exports = function(req, res, next) {
 	var builder = new Builder('.');
 	builder.copyAssetsTo('public');
 	builder.build(function (err, res) {
 		if (err) return next(err);
+		mkdir('public');
 		write('public/tomecats.js', res.require + res.js);
 		next();
 	});
